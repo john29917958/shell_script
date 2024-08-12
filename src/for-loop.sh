da0656a4b0146411145c560ee292059eca5ca355
TZU-CHAO WANG <john29917958@gmail.com>
2024-08-12 16:09:49 +0800

    Format and refactor scripts

--- src/for-loop.sh
+++ src/for-loop.sh
@@ -3,6 +3,6 @@
 echo "This is a for loop program. This will prints numbers from 1 to 10: "
 max=10
 for i in $(seq 1 $max); do
-	echo "$i"
+  echo "$i"
 done
 echo "Program exits."
--- src/nested-for-loop (nine-nine).sh
+++ src/nested-for-loop (nine-nine).sh
@@ -2,7 +2,8 @@
 
 echo "This is a nested for loop program. It implements the nine-nine table."
 for i in $(seq 1 9); do
-	for j in $(seq 1 $i); do
-		echo "$i x $j = $(($i * $j))"
-	done
+  for j in $(seq 1 $i); do
+    echo "$i x $j = $(($i * $j))"
+  done
 done
+echo "Program exits."
